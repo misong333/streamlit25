23379eae84a90c072abeea36fe55ea30b53624f6
misong333 <halmum0917@naver.com>
2025-05-14 14:33:34 +0900

    Update About.py

--- pages/About.py
+++ pages/About.py
@@ -2,6 +2,6 @@ import streamlit as st
 
 st.title("This app is under construction.")
 
-url="https://github.com/MK316/streamlit25/raw/main/images/image00.png"
+url="https://github.com/misong333/streamlit25/raw/main/images/877E6383-176B-4ABC-98D1-D06272AADD65.png"
 st.image(url, caption="Image link", width=300)  # width in pixels
 

872a850f7726620c890baa1c40dc2a8923ebab2c
misong333 <halmum0917@naver.com>
2025-05-14 15:04:46 +0900

    Update Vocabulary.py

--- pages/Vocabulary.py
+++ pages/Vocabulary.py
@@ -15,7 +15,7 @@ with tab1:
   st.markdown("### 📋 Word Frequency Table")
 
    # Load CSV from GitHub (update the link below)
-  url = "https://raw.githubusercontent.com/MK316/Digital-Literacy-Class/refs/heads/main/data/word_frequency.csv"
+  url = "https://github.com/misong333/streamlit25/raw/main/data/word_frequency%202.csv"
   df = pd.read_csv(url)
 
     # Show table only when button is clicked
